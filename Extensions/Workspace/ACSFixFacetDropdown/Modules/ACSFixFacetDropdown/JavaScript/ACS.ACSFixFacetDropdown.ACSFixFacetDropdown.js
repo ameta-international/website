5c378fadaeb0520af2473288f1fc7d73cbe3d672
Diego Papaterra <diego.papaterra@oracle.com>
2019-08-01 17:21:21 -0300

    Fix for filter dropdown

---
+++ Extensions/Workspace/ACSFixFacetDropdown/Modules/ACSFixFacetDropdown/JavaScript/ACS.ACSFixFacetDropdown.ACSFixFacetDropdown.js
@@ -0,0 +1,38 @@
+
+define(
+	'ACS.ACSFixFacetDropdown.ACSFixFacetDropdown'
+,   ['Facets.Browse.View'
+	]
+,   function (FacetsBrowseView
+	)
+{
+	'use strict';
+
+	return  {
+		mountToApp: function mountToApp (container)
+		{
+			// using the 'Layout' component we add a new child view inside the 'Header' existing view 
+			// (there will be a DOM element with the HTML attribute data-view="Header.Logo")
+			// more documentation of the Extensibility API in
+			// https://system.netsuite.com/help/helpcenter/en_US/APIs/SuiteCommerce/Extensibility/Frontend/index.html
+			
+			/** @type {LayoutComponent} */
+			var layout = container.getComponent('Layout');
+			
+			if(layout && !SC.isPageGenerator())
+			{
+				var self = this;
+				layout.on('afterShowContent', function(view) 
+				{ 
+					if(view === FacetsBrowseView.prototype.attributes.id)
+					{
+						var $myGroup = $('#facet-group');
+						$myGroup.on('show.bs.collapse','.collapse', function() {
+								$myGroup.find('.collapse.in').collapse('hide');
+						});
+					}
+				});
+			}
+		}
+	};
+});
